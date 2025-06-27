eeb806f3a500a82927e67946ee170c5e3ebed431
Felix Marty <Felix.Marty@amd.com>
2025-06-27 15:10:08 +0200

    add back profile script

---
+++ scripts/2_profile.sh
@@ -0,0 +1,43 @@
+#Usage:
+# ./2_profile.sh server
+# ./2_profile.sh prof
+
+mkdir -p results
+MODEL="amd/Mixtral-8x7B-Instruct-v0.1-FP8-KV"
+
+if [ $1 == "server" ] || [ $1 == "all" ]; then
+    echo "INFO: server"
+    vllm serve $MODEL \
+	--disable-log-requests \
+	--no-enable-prefix-caching \
+	--kv_cache_dtype fp8 \
+	--compilation-config '{"full_cuda_graph": true}'
+fi
+
+
+if [ $1 == "prof" ] || [ $1 == "all" ] ; then
+    until curl -s localhost:8000/v1/models > /dev/null; 
+    do
+	sleep 1
+    done
+    echo "INIFO: performance"
+    INPUT_LENGTH=128
+    OUTPUT_LENGTH=10
+    CONCURRENT=16
+    date=$(date +'%b%d_%H_%M_%S')
+    rpt=result_${date}.json
+    python /app/vllm/benchmarks/benchmark_serving.py \
+        --model $MODEL \
+        --dataset-name random \
+        --random-input-len ${INPUT_LENGTH} \
+        --random-output-len ${OUTPUT_LENGTH} \
+        --num-prompts $(( $CONCURRENT * 2 )) \
+        --max-concurrency $CONCURRENT \
+        --request-rate inf \
+        --ignore-eos \
+        --save-result \
+        --profile \
+        --result-dir ./results_with_profile/ \
+        --result-filename $rpt \
+        --percentile-metrics ttft,tpot,itl,e2el
+fi

9517a847defadd570457b4b7e1cc50fbeae8e309
Felix Marty <felix.marty@amd.com>
2025-07-04 16:31:07 +0000

    improve script

--- scripts/0_container.sh
+++ scripts/0_container.sh
@@ -1,8 +1,23 @@
 CONTAINER_NAME="vllm-container"
 DOCKER_IMG="rocm/vllm-dev:nightly_0610_rc2_0610_rc2_20250605"
 
-docker stop $CONTAINER_NAME
 
+running_container=$(docker ps -q --filter "name=$CONTAINER_NAME")
+
+if [ $running_container ]; then
+    echo "Stopping the already running $CONTAINER_NAME container"
+    docker stop $CONTAINER_NAME
+fi
+
+
+if ! test -f vllm/setup.py; then echo "WARNING: This script assumes it is launched from a directory containing a cloned vllm, but it was not found. Make sure vllm is cloned at ${PWD}/vllm."; fi
+
+echo "Starting a container based off $DOCKER_IMG..."
+echo "With the following mounted folders:"
+echo "$PWD/.hf_cache -> /root/.cache/huggingface/hub"
+echo "$PWD/.vllm_cache -> /root/.cache/vllm/"
+echo "$PWD -> /workspace"
+echo "$PWD/vllm -> /vllm-dev"
 
 # PYTORCH_ROCM_ARCH="gfx942" is useful to later restrict kernel compilation only for CDNA3 architecture (MI300),
 # speeding up compilation time.
